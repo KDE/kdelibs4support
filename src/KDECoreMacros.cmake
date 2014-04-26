b0338b7b1f0b654c3d4fe37a03dceb9aa8b6a269
Kevin Ottens <ervin@kde.org>
2014-04-26 12:42:00 +0200

    Have kde4_install_ts_files use ki18n_install_ts_files internally

--- src/KDECoreMacros.cmake
+++ src/KDECoreMacros.cmake
@@ -15,13 +15,6 @@ endmacro (KDE4_ADD_KCFG_FILES)
 # TODO: This belongs whereever the rest of the kde translation system belongs
 # Or in ECM, as it appears to be KDE independent, although it's odd to have such magic for TS files.
 macro(KDE4_INSTALL_TS_FILES _lang _sdir)
-   file(GLOB_RECURSE _ts_files RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} ${_sdir}/*)
-   foreach(_current_TS_FILES ${_ts_files})
-      string(REGEX MATCH "\\.svn/" _in_svn ${_current_TS_FILES})
-      if(NOT _in_svn)
-         get_filename_component(_subpath ${_current_TS_FILES} PATH)
-         install(FILES ${_current_TS_FILES} DESTINATION ${LOCALE_INSTALL_DIR}/${_lang}/LC_SCRIPTS/${_subpath})
-      endif(NOT _in_svn)
-   endforeach(_current_TS_FILES)
+   ki18n_install_ts_files(${_lang} ${_sdir})
 endmacro(KDE4_INSTALL_TS_FILES)
 
