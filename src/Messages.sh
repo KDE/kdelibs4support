f880e1108cfe58d42fa72be7ef15424ccd07aeaa
Alexander Potashev <aspotashev@gmail.com>
2014-06-26 00:47:01 +0400

    i18n: Do not extract color names containing spaces from rgb.txt
    CCMAIL: agateau@kde.org

--- src/Messages.sh
+++ src/Messages.sh
@@ -7,7 +7,22 @@ if [ -n "$lst" ] ; then
     $EXTRACTRC $lst >> rc.cpp
 fi
 
-awk '$1 ~ "[0-9]+" && $4 !~ "gr[ae]y" { print $4 }' kdeui/rgb.txt | LC_ALL=C sort -u | while read i ; do
+# The extracted strings will be used by KColorTable::readNamedColor(),
+# see src/kdeui/kcolordialog.cpp. Here we extract from rgb.txt
+# the same subset of color names as in KColorTable::readNamedColor(),
+# i.e. we throw away
+#  - color names containing spaces,
+#  - color names containing "gray" or "grey" as substrings.
+#
+# This filtering was introduced long before KDE Frameworks 5.0,
+# probably for early versions of KDE SC 4.x or earlier. The rationale
+# for this filtering and the reason why we keep it is the following:
+#   1. Most colors names containing spaces in rgb.txt have also their
+#        versions written without spaces but with exactly same RGB
+#        components.
+#   2. There are too many "gray/grey" colors, keeping them in the list
+#        would add only mess.
+awk '$1 ~ "[0-9]+" && $4 !~ "gr[ae]y" && NF == 4 { print $4 }' kdeui/rgb.txt | LC_ALL=C sort -u | while read i ; do
     echo "i18nc(\"color\", \"$i\");" | sed -e "s#!# #g" >> rc.cpp
 done
 
