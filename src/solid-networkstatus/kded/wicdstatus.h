f2b3113eaaededf745f5a7834dad4be48ed9ee30
Montel Laurent <montel@kde.org>
2015-02-25 08:54:38 +0100

    Remove /*reimp*/ we have Q_DECL_OVERRIDE

--- src/solid-networkstatus/kded/wicdstatus.h
+++ src/solid-networkstatus/kded/wicdstatus.h
@@ -32,9 +32,9 @@ class WicdStatus : public SystemStatusInterface
 public:
     WicdStatus( QObject *parent = 0 );
 
-    /* reimp */ Solid::Networking::Status status() const Q_DECL_OVERRIDE;
-    /* reimp */ bool isSupported() const Q_DECL_OVERRIDE;
-    /* reimp */ QString serviceName() const Q_DECL_OVERRIDE;
+    Solid::Networking::Status status() const Q_DECL_OVERRIDE;
+    bool isSupported() const Q_DECL_OVERRIDE;
+    QString serviceName() const Q_DECL_OVERRIDE;
 
 private Q_SLOTS:
     void wicdStateChanged();
