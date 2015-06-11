701e80a385995f9ab0c1336908e22d7d4edd715e
Burkhard Lück <lueck@hube-lueck.de>
2015-06-11 23:13:49 +0200

    Load correct translation catalog for kdebugdialog REVIEW:124071

--- src/kdebugdialog/main.cpp
+++ src/kdebugdialog/main.cpp
@@ -101,7 +101,7 @@ int main(int argc, char ** argv)
     migrate.setConfigFiles(QStringList() << QStringLiteral("kdebugrc"));
     migrate.migrate();
 
-    K4AboutData data( "kdebugdialog5", 0, ki18n( "KDebugDialog"),
+    K4AboutData data( "kdebugdialog5", "kdelibs4support", ki18n( "KDebugDialog"),
             "1.0", ki18n("A dialog box for setting preferences for debug output"),
             K4AboutData::License_GPL, ki18n("Copyright 1999-2009, David Faure <faure@kde.org>"));
     data.addAuthor(ki18n("David Faure"), ki18n("Maintainer"), "faure@kde.org");
